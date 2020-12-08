b08b5a2ee5ab34da66482d6514e2d271306d4b72
MihinMUD <73010893+MihinMUD@users.noreply.github.com>
2020-12-08 09:47:46 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -104,16 +104,40 @@ minetest.register_node("orenaite:orenaite_ore", {
 	groups = {cracky = 1},
 	drop = "orenaite:orenaite",
 })
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "orenaite:orenaite_ore",
-	wherein        = "default:stone",
-	clust_scarcity = 9 * 9 * 9,
-	clust_num_ores = 12,
-	clust_size     = 3,
-	y_max          = 31000,
-	y_min          = 1025,
-})
+	-- Coal
+
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "orenaite:orenaite_ore",
+		wherein        = "default:stone",
+		clust_scarcity = 8 * 8 * 8,
+		clust_num_ores = 9,
+		clust_size     = 3,
+		y_max          = 31000,
+		y_min          = 1025,
+	})
+
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "orenaite:orenaite_ore",
+		wherein        = "default:stone",
+		clust_scarcity = 8 * 8 * 8,
+		clust_num_ores = 8,
+		clust_size     = 3,
+		y_max          = 64,
+		y_min          = -31000,
+	})
+
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "orenaite:orenaite_ore",
+		wherein        = "default:stone",
+		clust_scarcity = 24 * 24 * 24,
+		clust_num_ores = 27,
+		clust_size     = 6,
+		y_max          = 0,
+		y_min          = -31000,
+	})
 
 
 
